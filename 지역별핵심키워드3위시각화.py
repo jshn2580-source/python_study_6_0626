d5189d6c5a1db25329dfe46b55dfcef0c22d25a2
jshn2580-source <jshn2580@gmail.com>
2026-06-29 21:39:08 +0900

    시리즈연산자

--- 지역별핵심키워드3위시각화.py
+++ 지역별핵심키워드3위시각화.py
@@ -24,11 +24,11 @@ df_region['상세지역'] = df_region['통합 분류1'].apply(
 
 df_region['키워드'] = df_region['키워드'].fillna('')
 # 빈문자열이라도 삽입
-all_region_names = list(df_region['상세지역'].dropna().unique())
+all_region_names = list(df_region['상세지역'].dropna().unique())  #지역리스트만들어줘 중복하지 말고
 
 
 def get_top_10_keywords(series):   
-    all_text = " ".join(series.astype(str))    
+    all_text = " ".join(series.astype(str))    #하나의 문자열로
     words = [word.strip() for word in all_text.replace(',', ' ').split() if word.strip()]
     top_10 = [item[0] for item in Counter(words).most_common(5)]
     return top_10
